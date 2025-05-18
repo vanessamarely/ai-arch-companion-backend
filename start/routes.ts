4820d333347c64ddc3b7f9a6a518b21c373a61dc
vanessamarely <vanessamarely@gmail.com>
2025-05-17 19:34:52 -0500

    route added

--- start/routes.ts
+++ start/routes.ts
@@ -11,9 +11,11 @@ import router from '@adonisjs/core/services/router'
 
 const HomeController = () => import('#controllers/home_controller')
 const LangGraphsController = () => import('#controllers/lang_graphs_controller')
+const GithubController = () => import('#controllers/github_controller')
 
 //routes
 
 router.get('/', [HomeController, 'index'])
 
 router.post('/agents/process', [LangGraphsController, 'handle'])
+router.post('/github/create-project', [GithubController, 'createProjectRepository'])
